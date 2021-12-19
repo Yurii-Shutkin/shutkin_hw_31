2566678dc64abd2d0d05495cd148ced67e181e4c
Yurii-Shutkin <itorik@ukr.net>
2021-12-19 23:32:55 +0200

    init

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 (() => {
 
-    const multiArr = [1, 2, [3, [4, [5], 6],[7, 8, [9, 0]]]];
+    const multiArr = [1, 2, [3, [4, [5], 6], [7, 8, [9, 0]]]];
     const simpleArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]; 
 
     function flatEmul(arr) {
